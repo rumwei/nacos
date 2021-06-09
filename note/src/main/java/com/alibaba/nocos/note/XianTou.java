e9b1ae8ef6ae2a2eb0478aebeb33bfd1ec0b77ff
guwei <guwei@fenbi.com>
2021-06-09 10:01:24 +0800

    add note module

---
+++ note/src/main/java/com/alibaba/nocos/note/XianTou.java
@@ -0,0 +1,12 @@
+package com.alibaba.nocos.note;
+
+/**
+ * 线头：即程序启动时的逻辑入口
+ */
+
+public class XianTou {
+    /**
+     * {@com.alibaba.nacos.core.cluster.ServerMemberManager}
+     *
+     */
+}
---
+++ note/src/test/java/com/alibaba/nocos/note/NoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.nocos.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
